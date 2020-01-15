9ff5ebf52fda1371cce727322bca45a15a9d3f47
jamesandrewpohadi <jamesandrewpohadi@gmail.com>
2020-01-16 00:46:58 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-mkdir dataset weights
+mkdir -p data/content data/style weights ckpt
 
 wget https://transformer-cds.s3-ap-southeast-1.amazonaws.com/vgg_normalised.pth
 wget https://transformer-cds.s3-ap-southeast-1.amazonaws.com/decoder.pth
 
 wget http://images.cocodataset.org/zips/val2014.zip
 unzip val2014.zip
-mv val2014 dataset/
+mv val2014 data/content/
 
 wget https://transformer-cds.s3-ap-southeast-1.amazonaws.com/kaggle.json
 mkdir -p ~/.kaggle
@@ -13,6 +13,6 @@ cp kaggle.json ~/.kaggle/
 
 kaggle competitions download -c painter-by-numbers -f train_2.zip
 unzip train_2
-mv train_2 style
+mv train_2 data/style
 
 pip3 install -r requirements.txt
